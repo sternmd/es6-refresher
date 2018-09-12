f0bd7fac301f0dc9c25e6b28a0bf4dcf482b603d
Maxwell Stern <maxwell.stern@makingwaves.com>
2018-09-12 15:59:09 -0500

    const & let

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 // const & let
+let name = 'Maxwell';
+let test;
+
+console.log(name);
+name = 'Jack';
+
+const nums = [1,2,3,4];
+
+const person = {
+  name: 'John',
+  age: 33
+}
+
+person.name = 'Jack'; // valid
+nums.push(5); // valid
 
 // arrow functions
 
